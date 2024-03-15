1506b7fe1ae90d3a4e4a137270672ddf3af7fa6b
nmjxblw <zchen386@ucsc.edu>
2024-03-15 02:52:40 -0700

    add moving credit text

---
+++ Assets/Scripts/Test Scripts/MovingText.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+public class MovingText : MonoBehaviour
+{
+    public float speed = 10f;
+    public IEnumerator Start()
+    {
+        yield return new WaitForSeconds(1);
+        StartCoroutine(MoveText());
+    }
+
+    public IEnumerator MoveText()
+    {
+        while (true)
+        {
+            transform.Translate(Vector3.up * Time.deltaTime * speed);
+            yield return null;
+        }
+    }
+}
